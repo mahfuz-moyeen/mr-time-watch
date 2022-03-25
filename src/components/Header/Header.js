05667301d545c1f931afaf692caba3f0dc38a55f
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-03-26 01:16:11 +0600

    add fake data

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-center'>Welcome to Mr Time shop</h1>
+        </div>
+    );
+};
+
+export default Header;
